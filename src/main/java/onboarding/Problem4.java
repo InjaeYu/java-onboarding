df40d4e6d7e98ea8eead1053087c5978d7d542f7
ijyu <ijyu93@gmail.com>
2022-11-04 20:31:26 +0900

    feat: problem4 해결

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,16 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder result = new StringBuilder();
+        for(char c : word.toCharArray()) {
+            if(c == ' ') {
+                result.append(c);
+            } else if(c >= 'a' && c <= 'z') {
+                result.append((char)('z' - c + 'a'));
+            } else if(c >= 'A' && c <= 'Z'){
+                result.append((char)('Z' - c + 'A'));
+            }
+        }
+        return result.toString();
     }
 }
