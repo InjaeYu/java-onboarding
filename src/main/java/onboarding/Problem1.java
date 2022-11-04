c2225b6e1e820b78a9fa247c0c1f894b3057f2f4
ijyu <ijyu93@gmail.com>
2022-11-04 21:10:30 +0900

    feat: problem1 최대값 연산 기능 구현
    예외사항에 대한 처리 추가 필요

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,37 @@
 package onboarding;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer = 0;
+        int pobiMax = maxResult(pobi);
+        int crongMax = maxResult(crong);
+
+        if(pobiMax > crongMax) {
+            answer = 1;
+        } else if (pobiMax < crongMax) {
+            answer = 2;
+        } else {
+            answer = 0;
+        }
+
         return answer;
+    }
+
+    private static int maxResult(List<Integer> list) {
+        int max = 0;
+        // TODO: 예외사항에 대한 처리 필요
+
+        for(Integer i : list) {
+            int sum = Stream.of(String.valueOf(i).split("")).mapToInt(Integer::parseInt).sum();
+            int mul = Stream.of(String.valueOf(i).split("")).mapToInt(Integer::parseInt).reduce(1, (a, b) -> a * b);
+            int calcMax = Math.max(sum, mul);
+            if(max < calcMax) {
+                max = calcMax;
+            }
+        }
+        return max;
     }
 }
