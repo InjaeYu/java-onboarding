07f3bc80b421b4eebeed7e0581171a8986454677
ijyu <ijyu93@gmail.com>
2022-11-04 21:37:05 +0900

    feat: problem3 시간 복잡도 무시 해결
    시간 복잡도를 생각하지 않는 가장 간단한 풀이 방법
    시간 복잡도를 생각해서 리팩터링 필요

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,13 @@
 package onboarding;
 
+import java.util.stream.Stream;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int i = 1; i <= number; i++) {
+            answer += Stream.of(String.valueOf(i).split("")).mapToInt(Integer::parseInt).filter(o -> o != 0 && o % 3 == 0).count();
+        }
         return answer;
     }
 }
