4c13b08027087ce312b58c5f0c8ce17f71990f10
ijyu <ijyu93@gmail.com>
2022-11-04 20:34:50 +0900

    feat: problem2 해결

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,36 @@
 package onboarding;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        int count = 0;
+        boolean reverse = false;
+        List<Character> charList = cryptogram.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
+        Character base = charList.get(0);
+
+        for (int i = 1; i < charList.size(); ) {
+            if (charList.get(i).equals(base)) {
+                count += 1;
+                charList.remove(i);
+            } else {
+                if (count == 0) {
+                    if (reverse) {
+                        reverse = false;
+                    }
+                    base = charList.get(i++);
+                } else {
+                    count = 0;
+                    reverse = true;
+                    charList.remove(--i);
+                    base = charList.get(i - 1);
+                }
+            }
+        }
+        if (count != 0) {
+            charList.remove(charList.size() - 1);
+        }
+        return charList.stream().map(String::valueOf).collect(Collectors.joining());
     }
 }
