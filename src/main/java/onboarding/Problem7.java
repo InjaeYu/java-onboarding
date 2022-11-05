950ad1b039dad486fa4eaf22efb9bd06634359b8
ijyu <ijyu93@gmail.com>
2022-11-05 15:59:46 +0900

    feat: problem7 방문 횟수에 따른 점수 할당 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,16 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    private static Map<String, Integer> friendsPointMap = new HashMap<>();
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        visitorsPoint(visitors);
         return answer;
+    }
+
+    private static void visitorsPoint(List<String> visitors) {
+        visitors.stream().forEach(k -> friendsPointMap.put(k, friendsPointMap.getOrDefault(k, 0) + 1));
     }
 }
